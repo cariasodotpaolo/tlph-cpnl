123bb2ad3ab4ac47d40644b36a2c9dc0f10a6768
Michael Paolo Cariaso <mpcariaso@gmail.com>
2017-03-15 23:03:37 +0800

    Fixed div content alignments.

--- php/footer.php
+++ php/footer.php
@@ -6,16 +6,14 @@
     <title></title>
 </head>
 <body>
-    <div id="footer-wrapper">
-        <div id="footer-content">
-            <p>&copy; 2014 Bong Bajo. All rights reserved.</p>
-            <br>
-            <p><strong>Interested in Timelapse Photography?</strong> Contact us:</p>
-            <p>timelapsephilippines@gmail.com<br>
-                support@timelapsephilippines.com</p>
-            <p>Web Development, Design and Administration By:</p>
-            <p>Michael Paolo Cariaso</p>
-        </div>
+    <div id="footer-content">
+        <p>&copy; 2014 Bong Bajo. All rights reserved.</p>
+        <br>
+        <p><strong>Interested in Timelapse Photography?</strong> Contact us:</p>
+        <p>timelapsephilippines@gmail.com<br>
+            support@timelapsephilippines.com</p>
+        <p>Web Development, Design and Administration By:</p>
+        <p>Michael Paolo Cariaso</p>
     </div>
 </body>
 </html>
