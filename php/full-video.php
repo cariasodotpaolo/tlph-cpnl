821ae7dd803a28116a0538b16be5965c2479f7b5
Michael Paolo Cariaso <mpcariaso@Enzos-iMac.local>
2018-07-30 18:16:12 +0800

    Added mayon video to carousel.

--- php/full-video.php
+++ php/full-video.php
@@ -9,9 +9,7 @@
     <div id="full-video-container">
         <div id="full-video-content">
             <p><span class="section-title">Projects</span></p><br>
-
-            <iframe width="900" height="360" src="https://www.youtube.com/embed/DvnAWc0vt6E" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-
+            
             <?php
             $string = file_get_contents("../data/menu-full-video.json");
             $json_a = json_decode($string, true);
