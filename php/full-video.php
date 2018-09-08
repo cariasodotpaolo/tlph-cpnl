a4a5bdb3c130b2f7a1426fe44ad67fb759c10c1c
Michael Paolo Cariaso <michaelc@middleware360.com>
2018-09-08 16:19:35 +0800

    Removed video splash image.

--- php/full-video.php
+++ php/full-video.php
@@ -9,7 +9,7 @@
     <div id="full-video-container">
         <div id="full-video-content">
             <p><span class="section-title">Projects</span></p><br>
-            
+
             <?php
             $string = file_get_contents("../data/menu-full-video.json");
             $json_a = json_decode($string, true);
@@ -20,7 +20,7 @@
                     $title = $video_a['title'];
 
                     echo '<p><a id="video-link" href="#" onclick="popUpVideo(\'' .$title .'\',\''. $videoUrl . '\')">';
-                    echo '<img id="video-image" width="600" height="240" src="../assets/splash/' . $imageFile . '"/></a></p>';
+                    // echo '<img id="video-image" width="600" height="240" src="../assets/splash/' . $imageFile . '"/></a></p>';
                 }
             ?>
         </div>
