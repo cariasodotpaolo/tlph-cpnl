d86221d425206acd064f555322a13f828da7cd36
Michael Paolo Cariaso <mpcariaso@gmail.com>
2018-09-09 15:21:51 +0800

    boot index page.

---
+++ coolchange/php/home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Timelapse Philippines</title>
+</head>
+<body>
+<div id="main-container" style="horiz-align: center;">
+    <?php include 'login/login.php';?>
+</div>
+</body>
+</html>
