61ed878247f81467a8eb5bf8bb6d1d0afb31ca8f
Michael Paolo Cariaso <mpcariaso@gmail.com>
2018-09-14 20:12:29 +0800

    project videos edit page.

--- coolchange/php/data/project-video-data.php
+++ coolchange/php/data/project-video-data.php
@@ -5,21 +5,22 @@ require_once "dbconfig/dbconfig.php";
 
         $sql = "SELECT id, title, video_url, position FROM project_videos order by position asc";
 
-        $sth = mysqli_query($sql);
-        $rows = array();
-        while($r = mysqli_fetch_assoc($sth)) {
-            $rows[] = $r;
-        }
+        $result = $mysqli->query($sql);
+        //$sth = mysqli_query($sql);
+        //$rows = array();
+        // while($r = mysqli_fetch_assoc($sth)) {
+        //     $rows[] = $r;
+        // }
         //echo "<p>video data</p>";
         //echo "<div> " . json_encode($rows) . "</div";
-        $jsonData = json_encode($rows);
-        $mysqli->close();
+        //$jsonData = json_encode($rows);
+
+
 
 ?>
 
 <!DOCTYPE html>
 <html>
-
 <head lang="en">
   <meta charset="utf-8" />
   <title>Timelpase Philippines</title>
@@ -31,7 +32,20 @@ require_once "dbconfig/dbconfig.php";
 
     <?php
         echo "<p>project-video-data.php</p>";
-        echo "<p>" . $jsonData . "</p>";
+        //echo "<p>" . $jsonData . "</p>";
+
+        while($row = $result->fetch_array())
+        {
+          $rows[] = $row;
+        }
+
+        foreach($rows as $row)
+        {
+          echo "<p>" . $row['video_url'] . "</p>";
+        }
+
+        $result->close();
+        $mysqli->close();
     ?>
   </div>
 </body>
