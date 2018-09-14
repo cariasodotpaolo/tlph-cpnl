3b6665cad325f547970e232c13af480611cbf260
Michael Paolo Cariaso <mpcariaso@gmail.com>
2018-09-14 19:33:57 +0800

    project videos edit page.

--- coolchange/php/data/project-video-data.php
+++ coolchange/php/data/project-video-data.php
@@ -10,9 +10,26 @@ require_once "dbconfig/dbconfig.php";
         while($r = mysqli_fetch_assoc($sth)) {
             $rows[] = $r;
         }
-        echo "<p>video data</p>";
-        echo "<div> " . json_encode($rows) . "</div";
-
+        //echo "<p>video data</p>";
+        //echo "<div> " . json_encode($rows) . "</div";
+        $jsonData = json_encode($rows);
         $mysqli->close();
 
 ?>
+
+<!DOCTYPE html>
+<html>
+
+<head lang="en">
+  <meta charset="utf-8" />
+  <title>Timelpase Philippines</title>
+  <!-- <script type="text/javascript" src="../../js/coolc.js"></script> -->
+</head>
+
+<body>
+  <div class="container">
+    <?php echo "<p>" . $jsonData . "</p>"; ?>
+  </div>
+</body>
+
+</html>
