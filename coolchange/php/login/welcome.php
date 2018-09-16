cc2a19c46f07efe3e768baf1829acf29cfeb7e66
Michael Paolo Cariaso <michaelc@middleware360.com>
2018-09-16 11:51:03 +0800

    debugging

--- coolchange/php/login/welcome.php
+++ coolchange/php/login/welcome.php
@@ -7,24 +7,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login/login.php");
     exit;
 }
-
-//require_once "dbconfig/dbconfig.php";
-
-        $sql2 = "SELECT id, title, video_url, position FROM project_videos order by position asc";
-
-        $stmt2 = $mysqli->prepare($sql2);
-
-        if($stmt2->execute()) {
-            $stmt2->store_result();
-
-            if($stmt2->num_rows > 0){
-                $stmt2->bind_result($id, $title, $videoUrl, $position);
-            } else {
-              echo "No project videos found.";
-            }
-        } else {
-              echo "Oops! Something went wrong. Please try again later.";
-        }
 ?>
 
 <!DOCTYPE html>
@@ -44,22 +26,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
               <a href="logout.php" class="btn btn-danger">Sign Out Of Dashboard</a>
           </div>
           <div>
-              <?php
-
-                  //echo '<p>project-video-data.php</p>';
-                  //echo "<p>" . $jsonData . "</p>";
-
-                  while ($stmt2->fetch()) {
-                      echo '<p>' . $title . '</p>';
-                  }
-
-                  $stmt2->free_result();
-                  $stmt2->close();
-                  // $mysqli->close();
-
-                  //include '/php/data/project-video-data.php';
-                  //include '../coolc-template.php';
-              ?>
+              <a href="../data/project-video-data.php" class="btn btn-warning">Edit Project Videos</a>
           </div>
     </body>
 </html>
