bdb7b30747ee887433d1c977b44e10235dde6aae
Michael Paolo Cariaso <mpcariaso@gmail.com>
2018-09-19 10:24:25 +0800

    project videos edit page.

--- coolchange/php/data/project-video-data.php
+++ coolchange/php/data/project-video-data.php
@@ -34,36 +34,9 @@ require_once "../dbconfig/dbconfig.php";
         while($row = $stmt->fetch_array(MYSQL_ASSOC)) {
             $myArray[] = $row;
         }
-        echo json_encode($myArray);
+        print json_encode($myArray);
 
         $stmt->free_result();
         $stmt->close();
         $mysqli->close();
 ?>
-
-<!DOCTYPE html>
-<html>
-    <head lang="en">
-      <meta charset="utf-8" />
-      <title>Timelpase Philippines</title>
-      <!-- <script type="text/javascript" src="../../js/coolc.js"></script> -->
-    </head>
-
-    <body>
-        <div>
-          <table id="langTable" data-toggle="table" data-url="/json/tabledata.json" data-sort-name="native" data-sort-order="desc" data-cache="false">
-            <thead>
-              <tr>
-                <th data-field="id">id</th>
-                <th data-field="language">Language</th>
-                <th data-field="native" data-formatter="formatNativeItem" data-align="center">Native</th>
-                <th data-field="level">Level</th>
-                <th data-field="looking">Looking</th>
-                <th data-field="operate" data-formatter="operateFormatter" data-align="center" data-events="operateEvents" data-width="100"></th>
-              </tr>
-            </thead>
-          </table>
-        </div>
-
-    </body>
-</html>
--- coolchange/php/project-videos.php
+++ coolchange/php/project-videos.php
@@ -7,6 +7,19 @@
   <meta charset="utf-8" />
   <title>Timelpase Philippines</title>
   <!-- <script type="text/javascript" src="../../js/coolc.js"></script> -->
+  <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+  <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.2.3/bootstrap-table.min.css" />
+  <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-switch/3.0.2/css/bootstrap2/bootstrap-switch.css"/>
+  <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/select2/3.5.0/select2.css">
+  <link rel="stylesheet" type="text/css" href="../../css/coolc.css" />
+
+  <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <script type="text/javascript" src="//netdna.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/bootstrap-switch/3.0.2/js/bootstrap-switch.js"></script>
+  <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.2.3/bootstrap-table.min.js"></script>
+  <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/select2/3.5.0/select2.js"></script>
+  <script type="text/javascript" src="../../js/coolc.js"></script>
 </head>
 
 <body>
@@ -61,7 +74,7 @@
           <thead>
             <tr>
               <th data-field="id">id</th>
-              <th data-field="title">Language</th>
+              <th data-field="title">Title</th>
               <!-- <th data-field="native" data-formatter="formatNativeItem" data-align="center">Native</th> -->
               <th data-field="videoUrl">Video URL</th>
               <th data-field="position">Position</th>
